482456e5fb92e6f66b6bc228a9ce6efb7da13ae7
zhengyutay <zy>
2020-07-08 18:49:21 +0800

    fix: retain externals

--- packages/webpack-externals-plugin/src/index.ts
+++ packages/webpack-externals-plugin/src/index.ts
@@ -4,6 +4,7 @@ import nodeExternals, {
   Options,
   WhitelistOption
 } from 'webpack-node-externals';
+import { ExternalsElement } from 'webpack';
 
 export default class WebpackExternalsPlugin {
   option: Options;
@@ -20,13 +21,19 @@ export default class WebpackExternalsPlugin {
       fn: (chain, { name, mode }) => {
         if (mode === 'development') {
           if (name === BUNDLER_TARGET_SERVER) {
+            const externals = chain.get('externals') as ExternalsElement;
             chain.externals(
-              nodeExternals({
-                whitelist: ([/^@shuvi\/app/] as WhitelistOption[])
-                  .concat(whitelist)
-                  .filter(Boolean),
-                ...otherOptions
-              }) as any
+              ([] as ExternalsElement[])
+                .concat(
+                  externals,
+                  nodeExternals({
+                    whitelist: ([/^@shuvi\/app/] as WhitelistOption[])
+                      .concat(whitelist)
+                      .filter(Boolean),
+                    ...otherOptions
+                  }) as ExternalsElement
+                )
+                .filter(Boolean)
             );
           }
         }
