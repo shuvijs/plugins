07c13baf3a3716583536ff0018b2b405e9b94729
zhengyutay <zy>
2020-07-09 17:53:36 +0800

    fix(server-externals-plugin): should not be environment specific

--- packages/server-externals-plugin/src/index.ts
+++ packages/server-externals-plugin/src/index.ts
@@ -17,26 +17,24 @@ export default class WebpackExternalsPlugin {
 
     api.tap<APIHooks.IHookBundlerConfig>('bundler:configTarget', {
       name: 'webpackExternalPlugin',
-      fn: (chain, { name, mode, helpers }) => {
-        if (mode === 'development') {
-          if (name === BUNDLER_TARGET_SERVER) {
-            helpers.addExternals(chain, (context, request, next) => {
-              const customCallback: any = (err: any, result: any) => {
-                if (!err && !result) {
-                  next(null, 'next');
-                } else {
-                  next(err, result);
-                }
-              };
+      fn: (chain, { name, helpers }) => {
+        if (name === BUNDLER_TARGET_SERVER) {
+          helpers.addExternals(chain, (context, request, next) => {
+            const customCallback: any = (err: any, result: any) => {
+              if (!err && !result) {
+                next(null, 'next');
+              } else {
+                next(err, result);
+              }
+            };
 
-              nodeExternals({
-                whitelist: ([/^@shuvi\/app/] as WhitelistOption[])
-                  .concat(whitelist)
-                  .filter(Boolean),
-                ...otherOptions
-              })(context, request, customCallback);
-            });
-          }
+            nodeExternals({
+              whitelist: ([/^@shuvi\/app/] as WhitelistOption[])
+                .concat(whitelist)
+                .filter(Boolean),
+              ...otherOptions
+            })(context, request, customCallback);
+          });
         }
         return chain;
       }
